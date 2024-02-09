b8d2e877902cf5cd513168d64805f0e3a8087154
user11245656565656 <92296230+lingoiy@users.noreply.github.com>
2024-02-09 21:41:16 +0500

    Update script.js

--- a_project/script.js
+++ a_project/script.js
@@ -3,13 +3,16 @@ var clog = console.log;
 var iahtml = pg.insertAdjacentHTML;
 var current = 0;
 var btn = pg.querySelector('.c')
+var ttl = pg.querySelector('title')
 const symbols = ["А","Б","В","Г","Д","Е","Ё","Ж","З","И","Й","К","Л","М","Н","О","П","Р","С","Т","У","Ф","Х","Ц","Ч","Ш","Щ","Ъ","Ы","Ь","Э","Ю","Я"];
 btn.addEventListener("click", function() {
   if (current == symbols.length - 1) {
     current = 0;
     btn.innerHTML = symbols[0];
+    ttl.innerHTML = symbols[0];
   } else {
     current += 1;
     btn.innerHTML = symbols[current];
+    ttl.innerHTML = symbols[current];
   };
 });
