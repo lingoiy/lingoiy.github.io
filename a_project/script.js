847e7d08224e0788c08c90257632155f1842db07
user11245656565656 <92296230+lingoiy@users.noreply.github.com>
2024-02-09 21:39:18 +0500

    Add files via upload

---
+++ a_project/script.js
@@ -0,0 +1,15 @@
+var pg = document;
+var clog = console.log;
+var iahtml = pg.insertAdjacentHTML;
+var current = 0;
+var btn = pg.querySelector('.c')
+const symbols = ["А","Б","В","Г","Д","Е","Ё","Ж","З","И","Й","К","Л","М","Н","О","П","Р","С","Т","У","Ф","Х","Ц","Ч","Ш","Щ","Ъ","Ы","Ь","Э","Ю","Я"];
+btn.addEventListener("click", function() {
+  if (current == symbols.length - 1) {
+    current = 0;
+    btn.innerHTML = symbols[0];
+  } else {
+    current += 1;
+    btn.innerHTML = symbols[current];
+  };
+});
