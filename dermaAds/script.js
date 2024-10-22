2b6d323a998aa4e33c3ceaa30fb27b0019c9730d
user11245656565656 <92296230+lingoiy@users.noreply.github.com>
2024-10-22 11:30:37 +0500

    Update script.js

--- dermaAds/script.js
+++ dermaAds/script.js
@@ -6,3 +6,7 @@ function playend() {
   document.getElementById("overlay2").style.display = "block";
   setTimeout(function(){closeBtn.style.display = "block";}, 5000);
 }
+function pickLink() {
+  let link = "https://youtube.com/embed/" + videos[Math.floor(Math.random() * 6)];
+  window.open(link, "_blank", "width=480;height=270;");
+}
